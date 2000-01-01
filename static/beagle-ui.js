f8559a3c1a145560076a120e44319caec52d04c5
Jason Kridner <jdk@ti.com>
2000-01-01 01:51:59 +0000

    index.html: initial dynamic connection status

--- static/beagle-ui.js
+++ static/beagle-ui.js
@@ -32,12 +32,32 @@ $(document).ready(function(){
         $.get('/static/connect-status.html', function(data){
             $('#connect-status').replaceWith(data);
         });
+
+        var connected = '' +
+            '<div id="connect-status">' +
+            '    <div class="browser-connected">' +
+            '        <img alt="Connected" src="/static/images/green_check.png" border="0">' +
+            '        <div id="browser-content"><strong>Your board is connected!</strong><br>' +
+            '            Detected:  BeagleBoard Version 1.0Download ISO' +
+            '        </div>' +
+            '    </div>' +
+            '</div>';
+        var i = 0;
+        var serversToTry = [
+            window.location.host,
+            '192.168.7.2',
+            'beaglebone.local',
+            'beaglebone-2.local'
+        ];
+        testForConnection();
+        function testForConnection() {
+            setTargetAddress(serversToTry[i], function () {
+                if(typeof _bonescript != 'undefined') {
+                    $('#connect-status').replaceWith(connected);
+                } else {
+                    setTimeout(testForConnection, 1000);
+                }
+            });
+        }
     }
 });
-
-// <div class="browser-connected">
-// <img alt="Connected" src="/static/images/green_check.png" border="0">
-// <div id="browser-content"><strong>Your board is connected!</strong><br>
-//Detected:  BeagleBoard Version 1.0Download ISO
-// </div>
-//</div>
