c378b04186a609b0e4af2106b914db4577780e36
Jason Kridner <jdk@ti.com>
2013-04-06 16:11:15 -0400

    index.html: more clean-up

--- static/beagle-ui.js
+++ static/beagle-ui.js
@@ -1,16 +1,26 @@
 var name = "#floatMenu";  
 var menuYloc = null;  
   
-$(document).ready(function(){  
-    menuYloc = parseInt($(name).css("top").substring(0,$(name).css("top").indexOf("px")))  
-    $(window).scroll(function () {   
-        var offset = menuYloc+$(document).scrollTop()+"px";  
-        $(name).animate({top:offset},{duration:500,queue:false});  
-    });  
+$(document).ready(function(){
+    if($(name).length) {
+        menuYloc = parseInt($(name).css("top").substring(0,$(name).css("top").indexOf("px")));
+        $(window).scroll(function () {
+            var offset = menuYloc+$(document).scrollTop()+"px";
+            $(name).animate({top:offset},{duration:500,queue:false});
+        });
+    }
 });  
 
 $(function() {
-    $("#accordion").accordion({
-        collapsible: true
-    });
+    if($('#accordian').length) {
+        $("#accordion").accordion({
+            collapsible: true
+        });
+    }
+});
+
+$(document).ready(function(){
+    if($('#side-menu').length) {
+        $('#side-menu').load('/static/side-menu.html');
+    }
 });
