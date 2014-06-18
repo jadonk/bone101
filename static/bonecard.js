a2839b26436f6bcacdcb6180f222608612db5a0d
Jason Kridner <github@hangerhead.com>
2014-06-18 15:40:39 -0400

    Create bonecard.js
    Based on http://jsfiddle.net/jkridner/6BM78/ and http://jsfiddle.net/jkridner/UWtMb/

---
+++ static/bonecard.js
@@ -0,0 +1,61 @@
+$('.bonecard').css("cursor", "pointer");
+$('.bonecard').click(function() {
+    $(this).toggleClass('zoomed');
+});
+
+OAuth.initialize('t4Qxz2lcwB10Qgz_iXZwNjsZ1w4');
+
+$('#connect').click(function() {
+    OAuth.popup('github', function(err, result) {
+        console.log(err);
+        auth = result;
+    });
+});
+
+$('#edit').click(function() {
+    $.getScript('http://mindmup.github.io/bootstrap-wysiwyg/external/jquery.hotkeys.js', loadWysiwyg);
+});
+
+function loadWysiwyg() {
+    $.getScript('http://mindmup.github.io/bootstrap-wysiwyg/bootstrap-wysiwyg.js', enableEdit);
+}
+
+function enableEdit() {
+    $('#editor').wysiwyg();
+}
+
+var gistData = {
+    "description": "Bonecard tutorial",
+    "public": true,
+    "files": {
+    }
+};
+
+function makeGist() {
+    var url = "https://api.github.com/gists";
+    gistData.files["bonecard.html"] = {
+        "content": $('#editor').cleanHtml()
+    };
+    var mypost = {
+        type: "POST",
+        url: url,
+        data: JSON.stringify(s.testData),
+        success: onsuccess,
+        dataType: "json"
+    };
+    if(auth) {
+        mypost.headers = {
+            "Authorization": 'token ' + auth.access_token
+        };
+    }
+    console.log("Doing post: " + JSON.stringify(mypost));
+    $.ajax(mypost).fail(onfail);
+    
+    function onsuccess(response) {
+        console.log('success: ' + JSON.stringify(response));
+    }
+    
+    function onfail(response) {
+        console.log('fail: ' + JSON.stringify(response));
+    }
+};
