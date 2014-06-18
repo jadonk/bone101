a1b64cfd71a49fceb00781ecfdea2c4f70ddb29c
Jason Kridner <github@hangerhead.com>
2014-06-18 18:10:18 -0400

    Update bonecard.js
    Move code into document ready function

--- static/bonecard.js
+++ static/bonecard.js
@@ -1,47 +1,51 @@
-$(".bonecard-list .bonecard").each(function(index) {
-    var item = $(this);
-    var anchor = item.find("a").first();
-    var gistid = anchor.attr("gistid");
-    var gisturl = "https://api.github.com/gists/" + gistid;
-    var gistrequest = {
-        type: "GET",
-        url: gisturl,
-        success: gistsuccess,
-        dataType: "json"
-    };
-    console.log('request: ' + JSON.stringify(gistrequest));
-    $.ajax(gistrequest).fail(gistfail);
-    
-    function gistsuccess(response) {
-        console.log('success: ' + JSON.stringify(response));
-        anchor.replaceWith(response.files["cover.html"].content);
-        item.show();
-    }
-    
-    function gistfail(response) {
-        console.log('fail: ' + JSON.stringify(response));
-    }
-});
-$('.bonecard').css("cursor", "pointer");
+$( document ).ready(init);
 
-// TODO: This isn't the right way to zoom, just a placeholder
-// URL needs to be replaced
-$('.bonecard').click(function() {
-    $(this).toggleClass('bonecard-zoomed');
-});
-
-OAuth.initialize('t4Qxz2lcwB10Qgz_iXZwNjsZ1w4');
-
-$('#connect').click(function() {
-    OAuth.popup('github', function(err, result) {
-        console.log(err);
-        auth = result;
+function init() {
+    $(".bonecard-list .bonecard").each(function(index) {
+        var item = $(this);
+        var anchor = item.find("a").first();
+        var gistid = anchor.attr("gistid");
+        var gisturl = "https://api.github.com/gists/" + gistid;
+        var gistrequest = {
+            type: "GET",
+            url: gisturl,
+            success: gistsuccess,
+            dataType: "json"
+        };
+        console.log('request: ' + JSON.stringify(gistrequest));
+        $.ajax(gistrequest).fail(gistfail);
+        
+        function gistsuccess(response) {
+            console.log('success: ' + JSON.stringify(response));
+            anchor.replaceWith(response.files["cover.html"].content);
+            item.show();
+        }
+        
+        function gistfail(response) {
+            console.log('fail: ' + JSON.stringify(response));
+        }
     });
-});
+    $('.bonecard').css("cursor", "pointer");
 
-$('#edit').click(function() {
-    $.getScript('http://mindmup.github.io/bootstrap-wysiwyg/external/jquery.hotkeys.js', loadWysiwyg);
-});
+    // TODO: This isn't the right way to zoom, just a placeholder
+    // URL needs to be replaced
+    $('.bonecard').click(function() {
+        $(this).toggleClass('bonecard-zoomed');
+    });
+    
+    OAuth.initialize('t4Qxz2lcwB10Qgz_iXZwNjsZ1w4');
+    
+    $('#connect').click(function() {
+        OAuth.popup('github', function(err, result) {
+            console.log(err);
+            auth = result;
+        });
+    });
+    
+    $('#edit').click(function() {
+        $.getScript('http://mindmup.github.io/bootstrap-wysiwyg/external/jquery.hotkeys.js', loadWysiwyg);
+    });
+}
 
 function loadWysiwyg() {
     $.getScript('http://mindmup.github.io/bootstrap-wysiwyg/bootstrap-wysiwyg.js', enableEdit);
