75a7e7d3893715aecceed5fd5d2acdc2649d0bd2
Diego Turcios <diegoturciostc@gmail.com>
2014-06-23 01:41:23 -0600

    Adding new files. Created works. Having issues showing the display.

---
+++ Support/GSOC/app/js/bonecard.js
@@ -0,0 +1,112 @@
+$(document).ready(init);
+
+function init() {
+    function getParameterByName(name) {
+        name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
+        var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
+        results = regex.exec(location.search);
+        return results == null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
+    }
+    
+    if(location.hash) {
+        console.log(location.hash);
+        var gistid = location.hash.substring(1);
+        $(".bonecard-list").each(function(index) {
+            var list = $(this);
+            console.log("Replacing gistid " + list.attr("gistid") +
+                        "with " + gistid);
+            list.attr("gistid", gistid);
+        });
+    }
+    
+    
+    
+    $(".bonecard-list").each(function(index) {
+        var gistid = getParameterByName('gistid');
+        
+        //var list = $(this);
+        //var gistid = list.attr("gistid");
+        if(gistid) {
+            var gisturl = "https://api.github.com/gists/" + gistid;
+            var gistrequest = {
+                type: "GET",
+                url: gisturl,
+                success: gistsuccess,
+                dataType: "json"
+            };
+            console.log('request: ' + JSON.stringify(gistrequest));
+            $.ajax(gistrequest).fail(gistfail);
+        }
+
+        function gistfail(response) {
+            console.log('fail: ' + JSON.stringify(response));
+        }
+        
+        function getFileCards(obj){
+            objFiles = [];
+            $.each(obj, function(key, value) {
+                var str = obj[key].filename;
+                if (str.indexOf("CARD_") >= 0){
+                    objFiles.push(obj[key])
+                }
+            });
+            return objFiles;
+        }
+        
+        function addCards(objFiles){
+            $("#cardList");
+            len=100/objFiles.length;
+            mult=100*objFiles.length;
+            $(".box").css({"height":"100%","float":"left","width":len+"%"});
+            $(".bonecard-list").css({"height":"100%","float":"left","width":mult+"%"});
+            
+            for(i=0;i<objFiles.length;i++){
+                $("#cardList").append('<li class="box" id='+i+'><div class="bonecard"></div></li>');
+            }
+            
+        }
+        
+        function gistsuccess(response) {
+            console.log('success: ' + JSON.stringify(response));
+            obj = response.files;
+            var objFiles = getFileCards(obj);
+            
+            //list.replaceWith(response.files["list.html"].content);
+            addCards(objFiles);
+            /*$(".bonecard").each(function(index) {
+                console.log('found a bonecard');
+                var card = $(this);
+                var gistid = card.attr("gistid");
+                if(gistid) {
+                    var gisturl = "https://api.github.com/gists/" + gistid;
+                    var gistrequest = {
+                        type: "GET",
+                        url: gisturl,
+                        success: gistsuccess,
+                        dataType: "json"
+                    };
+                    console.log('request: ' + JSON.stringify(gistrequest));
+                    $.ajax(gistrequest).fail(gistfail);
+                }        
+
+                function gistsuccess(response) {
+                    console.log('success: ' + JSON.stringify(response));
+                    card.replaceWith('<div class="bonecard">\n' +
+                        response.files["cover.html"].content +
+                        '\n</div>'
+                    );
+                    card.show();
+                }
+            });*/
+            $('.bonecard').css("cursor", "pointer");       
+            $('.bonecard').click(function() {
+                // TODO: This isn't the right way to zoom, just a placeholder
+                // URL needs to be replaced
+                $(this).toggleClass('bonecard-zoomed');
+            });
+            //list.show();
+        }
+    });
+    
+   
+};
---
+++ Support/GSOC/app/js/createOptionForm.js
@@ -0,0 +1,105 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+function deleteUi(list,listActive,arrayList,editor){
+    sizeList = list.find("li").size();
+    if(sizeList > 1){
+        var id=listActive.attr("id");
+        var text=listActive.text();
+        var numId=parseInt(id)-1;
+        $("li:has('a'):contains("+text+")").remove();
+        arrayList.splice(numId,1)
+        $('.summernote').eq(editor).code(arrayList[0]);
+        sizeList = list.find("li").size();
+        for(i=0;i<sizeList;i++){
+            x=i+1;
+            if (i == 0){
+                list[0].children[i].outerHTML='<li class="active" id='+x+'><a href="#">Page '+x+'</a></li>';
+            }
+            else{
+                list[0].children[i].outerHTML='<li class="" id='+x+'><a href="#">Page '+x+'</a></li>';
+            }
+        }
+       
+    }
+}
+
+
+function addNewElement(list,listActive,arrayList,editor){
+    var Psize= list.find('li');
+    listActive.removeClass('active')
+    Psize = Psize.size()+1;
+    list.append('<li class="active" id='+Psize+'><a href="#">Page '+Psize+'</a></li>');
+    var Prevalue= $('.summernote').eq(editor).code();
+    arrayList.push(Prevalue);
+    $('.summernote').eq(editor).code("");
+}
+            
+function changeElement(ids,listActive,arrayList,editor,allThis){
+    var numids=parseInt(ids);
+    var Prevalue= $('.summernote').eq(editor).code();
+    var id=listActive.attr("id");
+    var numId=parseInt(id);
+    arrayList[numId-1]=Prevalue;
+    listActive.removeClass('active')
+    allThis.addClass('active');
+    $('.summernote').eq(editor).code(arrayList[numids-1])    
+}           
+            
+function checkTab(list,arrayList,editor){
+    var Prevalue= $('.summernote').eq(editor).code();
+    if(Prevalue.length > 11){
+        var Psize= list.find('li');
+        Psize = Psize.size();
+        if(arrayList.length <  Psize){
+            arrayList.push(Prevalue);
+        }
+    }
+    if(editor == 1){
+        $.cookie('preReqObject', arrayList,{ expires: 1, path: '/' });
+    }
+    else if(editor ==2){
+        $.cookie('preReqHD', arrayList,{ expires: 1, path: '/' });
+    }
+} 
+
+
+
+var create_Json = function createJson(pagesPreReq,pagesHDReq){
+    var tabOne=$('.summernote').eq(0).code();
+    var tabCode=four= $('.summernote').eq(3).code();
+    var tabAdditional = $('.summernote').eq(4).code();
+                
+    Jfile = {
+        "description": "Bone101 Tutorial",
+        "public": true,
+        "files": {
+            "CARD_AD_01.html":{ "content": tabAdditional},
+            "CARD_CD_01.html": {"content": tabCode}
+                         //"CARD_HD_01.md": {"content": three},
+                         //"CARD_PRE_01.md":{ "content": two},
+                         //"CARD_IN_01.md": {"content": tabOne}
+            }};
+                //Jfile=JSON.stringify(Jfile);   
+                //JfilePre = Jfile;
+    obj={};            
+    for(i=0;i<pagesPreReq.length;i++){
+        name="CARD_PRE_0"+(i+1)+".html";
+        obj={"content": pagesPreReq[i]};
+        Jfile["files"][name]=obj;
+    }
+    for(i=0;i<pagesHDReq.length;i++){
+        name="CARD_HD_0"+(i+1)+".html";
+        obj={"content": pagesHDReq[i]};
+        Jfile["files"][name]=obj;
+    }
+    obj={"content": tabOne};
+    Jfile["files"]["CARD_IN_01.html"]=obj;
+    
+     //return JSON.stringify(Jfile);
+    return Jfile;
+}
