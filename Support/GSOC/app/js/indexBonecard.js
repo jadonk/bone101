566a86244f81f089342dcbccfa0fd8314f29b42a
dturcios <dturcios@HCE-ECM-002.us.adler.corp>
2014-07-23 15:25:36 -0600

    Removing the > from index

--- Support/GSOC/app/js/indexBonecard.js
+++ Support/GSOC/app/js/indexBonecard.js
@@ -48,7 +48,7 @@ function init() {
                     console.log('success: ' + JSON.stringify(response));
                     console.log('Response id: '+ response.id);
                     link='<a href="tutorial.html?gistid='+response.id+'">';
-                    newDiv='<div class="bonecard">'+ response.files["CARD_Preview.html"].content +'></div></a>';
+                    newDiv='<div class="bonecard">'+ response.files["CARD_Preview.html"].content +'</div></a>';
                     link=link+newDiv;
                     card.replaceWith(link);
                     card.show();
