559c1ad17e4ddc2af94157df0530d1fef0a6b2c5
ebadawy <ehalbadawy93@gmail.com>
2015-08-09 15:39:05 +0200

    Make update_ace_editor_id_val func global

--- static/bonecard.js
+++ static/bonecard.js
@@ -110,15 +110,6 @@ function init() {
             update_ace_editor_id_val();
         });
 
-    function update_ace_editor_id_val() {
-        ace_editor = document.getElementsByClassName('slick-active')[0]
-            .getElementsByClassName('ace_editor')[0];
-        if (typeof ace_editor != 'undefined')
-            ace_editor_id = ace_editor.id;
-        else
-            ace_editor_id = -1;
-    }
-
     // check if the tutorial is not in the local storage do an ajax request to get & save it
     // then call preview_tutorial function with tutorial data.
     if (tutorial == null) {
