ff23925050cba0afb88bab3d923c61f4909f6f57
Jason Kridner <jdk@ti.com>
2016-07-25 11:45:13 -0400

    fsm: try to force fsmsave.js to show up

--- Support/bone101/UI/fsm/fsmsave.js
+++ Support/bone101/UI/fsm/fsmsave.js
@@ -1,1 +1,3 @@
+---
+---
 fsmsave={"nodes":[{"x":138,"y":94,"text":"test","isAcceptState":false},{"x":286,"y":155,"text":"test2","isAcceptState":false}],"links":[{"type":"Link","nodeA":0,"nodeB":1,"text":"path","lineAngleAdjust":0,"parallelPart":0.5,"perpendicularPart":0}]};
