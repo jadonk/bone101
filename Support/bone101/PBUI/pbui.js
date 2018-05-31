4a9841c44d1db91e224b3d49bd22379bc7e76ea3
vaishnav98 <mavaishnav007@gmail.com>
2018-06-01 00:51:24 +0530

    Revert "change voltage level to 3.3"
    This reverts commit baf1df34719c71f1592701922ca53f1ad4b77d7e.

--- Support/bone101/PBUI/pbui.js
+++ Support/bone101/PBUI/pbui.js
@@ -1350,9 +1350,9 @@ var UI = (function() {
             //all graph properties
             var graph = {
                 xWidth: 360,
-                yWidth: 297,
+                yWidth: 250,
                 zeroX: axisStartX,
-                zeroY: axisStartY + 200,
+                zeroY: axisStartY + 160,
                 interval: 0,
                 zoomVal: [.125, .25, .5, 1],
                 zoomIndex: 3,
@@ -1426,12 +1426,12 @@ var UI = (function() {
 
             //y ticks
             var y = 0;
-            var countY = ;
+            var countY = 5;
             var ynum = 0;
-            var volt = 3.3;
+            var volt = 5;
             var text;
             while (y <= graph.yWidth) {
-                if (countY % 3 === 0) {
+                if (countY % 5 === 0) {
                     canvas.Graph.ctx.beginPath();
                     canvas.Graph.ctx.moveTo(graph.zeroX - 10, graph.zeroY - y);
                     canvas.Graph.ctx.lineTo(graph.zeroX + 5, graph.zeroY - y);
@@ -1444,10 +1444,7 @@ var UI = (function() {
                     canvas.Graph.ctx.lineWidth = 2;
                     canvas.Graph.ctx.stroke();
                 }
-                if(countY > 53)
-			y+=9;
-		else
-			y+=3;
+                y += 10;
                 countY += 1;
             };
             canvas.Graph.ctx.fillStyle = "black";
@@ -1455,20 +1452,12 @@ var UI = (function() {
             while (ynum <= graph.yWidth && volt >= 0) {
                 text = (volt * graph.zoom()).toFixed(1).toString();
                 if (text == "0.0") {
-                    canvas.Graph.ctx.fillText(text, graph.zeroX - 20, graph.zeroY - graph.yWidth + ynum + 15);
+                    canvas.Graph.ctx.fillText(text.toString(), graph.zeroX - 20, graph.zeroY - graph.yWidth + ynum + 15);
                 } else {
-	            if (text == "1.8" || text == "3.3"){
-	            		canvas.Graph.ctx.fillStyle = "blue";
-                    		canvas.Graph.ctx.font = '10pt Lucinda Grande';
-		    }
-		    else{
-	            		canvas.Graph.ctx.fillStyle = "black";
-                    		canvas.Graph.ctx.font = '8pt Lucinda Grande';
-		    }
-                    canvas.Graph.ctx.fillText(text, graph.zeroX - 30, graph.zeroY - graph.yWidth + ynum +2);
+                    canvas.Graph.ctx.fillText(text.toString(), graph.zeroX - 30, graph.zeroY - graph.yWidth + ynum +2);
                 }
-                ynum += 27;
-                volt -= 0.3;
+                ynum += 50;
+                volt -= 1;
             };
             return xyAxis;
         })();
